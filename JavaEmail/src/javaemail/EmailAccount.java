b696da1561c498f7730685e78bdd250e1b7bcf2c
Edwin Gabriel Portillo <weaselSSH@gmail.com>
2025-10-17 10:31:09 -0600

    arreglos

--- JavaEmail/src/javaemail/EmailAccount.java
+++ JavaEmail/src/javaemail/EmailAccount.java
@@ -1,76 +1,74 @@
 package javaemail;
 
 import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class EmailAccount {
-    
+
     private String direccionEmail;
     private String password;
     private String nombreUsuario;
     private Email[] inbox;
-    
-    public EmailAccount(String direccionEmail, String password, String nombreUsuario){
+
+    public EmailAccount(String direccionEmail, String password, String nombreUsuario) {
         this.direccionEmail = direccionEmail;
         this.nombreUsuario = nombreUsuario;
         this.password = password;
         this.inbox = new Email[10];
-        
+
     }
-    
-    public String getDireccionEmail(){
+
+    public String getDireccionEmail() {
         return direccionEmail;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
-    
-    public String nombreUsuario(){
+
+    public String nombreUsuario() {
         return nombreUsuario;
     }
-    
-    
-    public Boolean recibirEmail(Email em){
-        for (int i=0; i<=inbox.length; i++){
-            if (inbox[i] == null){
+
+    public boolean recibirEmail(Email em) {
+        for (int i = 0; i < inbox.length; i++) {
+            if (inbox[i] == null) {
                 inbox[i] = em;
-                
+
                 return true;
             }
         }
         return false;
     }
-    
-    public static void printInbox (){
-        Calendar fecha = Calendar.getInstance();
-        System.out.println("Fecha: %tF %tT%n%n"+ fecha +fecha);
-        
-        int total = 0;
-        int sinLeer =0;
-        
-        
-        for (int i=0; i<inbox.length; i++){
-            Email em= inbox[i];
-            
-            if (em != null){
-                total++;
-                String estado = em.isLeido() ? "LEÍDO": "SIN LEER";
-                
-                if (em.isLeido())sinLeer++;
-                System.out.println("%d – %s – %s – [%s]%n"+ i +em.getEmisor() +em.getAsunto() +estado);
-            }
-            System.out.println("Total de correos: %d%n"+ total);
-            System.out.println("Correos sin leer  %d%n"+sinLeer);
-        }
-        
-        
-    }
-    
-    public static void leerEmail (int pos){
-        
-    }
-    
 
-    
-    
+    public String printInbox() {
+                Calendar fecha = Calendar.getInstance();
+        System.out.println("Fecha: %tF %tT%n%n" + fecha + fecha);
+
+        int total = 0;
+        int sinLeer = 0;
+
+        for (int i = 0; i < inbox.length; i++) {
+            Email em = inbox[i];
+
+            if (em != null) {
+                total++;
+                String estado = em.isLeido() ? "LEÍDO" : "SIN LEER";
+
+                if (em.isLeido()) {
+                    sinLeer++;
+                }
+                System.out.println("%d – %s – %s – [%s]%n" + i + em.getEmisor() + em.getAsunto() + estado);
+            }
+            System.out.println("Total de correos: %d%n" + total);
+            System.out.println("Correos sin leer  %d%n" + sinLeer);
+            
+            
+    }
+
+    public void leerEmail(int pos) {
+        if (inbox[pos] != null) {
+            inbox[pos].leer();
+        }
+    }
 }
