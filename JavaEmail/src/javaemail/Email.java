a6264607237b68968bfc2a0eddaddde786b9a55c
Edwin Gabriel Portillo <weaselSSH@gmail.com>
2025-10-17 08:25:06 -0600

    si

---
+++ JavaEmail/src/javaemail/Email.java
@@ -0,0 +1,11 @@
+package javaemail;
+
+import java.util.Calendar;
+
+public class Email {
+    String emisor;
+    String asunto;
+    String contenido;
+    boolean leido;
+    Calendar fechaEnvio;
+}
