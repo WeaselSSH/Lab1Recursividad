8de0d2c19fea81ef96e27685d12fe420a71db443
NajmahZablah <najmahzablah@gmail.com>
2025-10-17 09:20:11 -0600

    Mensaje vacio

--- JavaEmail/src/javaemail/Email.java
+++ JavaEmail/src/javaemail/Email.java
@@ -1,11 +1,69 @@
 package javaemail;
 
 import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
-public class Email {
-    String emisor;
-    String asunto;
-    String contenido;
-    boolean leido;
-    Calendar fechaEnvio;
+public class Email extends JavaEmail {
+
+    // definir atributos
+    public String emisor;
+    public String asunto;
+    public String contenido;
+    public boolean leido;
+    public Calendar fechaEnvio;
+    
+    // Crear el constructor
+    public Email(String emisor, String asunto, String contenido) {
+    this.emisor = emisor;
+    this.asunto = asunto;
+    this.contenido = contenido;
+    
+    // Inicializar leido en false
+    this.leido = false;
+    
+    // Asignar la fecha actual
+    this.fechaEnvio = Calendar.getInstance();
+    
+    }
+    
+    // Crear métodos (getters) para obtener cada atributo
+    public String getEmisor() {
+        return emisor;
+    }
+    
+    public String getAsunto() {
+        return asunto;
+    }
+    
+    public String getContenido() {
+        return contenido;
+    }
+    
+    public boolean getLeido() {
+        return leido;
+    }
+    
+    public Calendar getFechaEnvio() {
+        return fechaEnvio;
+    }
+    
+    // Función leido()
+    public void marcarLeido() {
+        this.leido = true;
+    }
+    
+    public boolean leer() {
+        this.leido = true;
+        return this.leido;
+    }
+    // Función print()
+    public String print() {
+        SimpleDateFormat fecha = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String estado = leido ? "Leido" : "Sin Leer";
+        return "De: ["+emisor+"] "
+             + "\nAsunto: ["+asunto+"] "
+             + "\nContenido: ["+contenido+"] "
+             + "\nFecha: ["+fecha.format(fechaEnvio.getTime())+"] "
+             + "\nEstado: ["+estado+"]";
+    }
 }
